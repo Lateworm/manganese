95eda1dc67b6e8d3ea68607c873c7d4efa959fe4
Lateworm <travis.rempel@gmail.com>
2022-09-05 19:22:16 -0700

    Add Spotify Url import

--- lib/io/import.rb
+++ lib/io/import.rb
@@ -1,17 +1,22 @@
 # rails c
 # load 'lib/io/import.rb'
 
-# CSV must by shrunk
+# CSV MUST BE SHRUNK!!!!
+# TODO: trim whitespace off all fields
+# TODO: validate URLs
+# TODO: find duplicates / data clenaup suite
 
 require 'csv'
 rows = CSV.parse(File.read('lib/io/recommendations.csv'), headers: true)
 bad_rows = []
 
 rows.each_with_index do |row, i|
-  if row['Artist'].length > 0 && row['Album'].length > 0
+  if row['Artist'].length > 0 && row['Album'].length > 0 && row['Spotify'].length
     # based on RecommendationsController#create ... should we just use that??
     artist = Artist.find_or_create_by(name: row['Artist'])
     album = Album.find_or_create_by(name: row['Album'], artist_id: artist.id)
+    spotify = Service.find_or_create_by(name: 'Spotify');
+    spotify_source = Source.find_or_create_by(service_id: spotify.id, album_id: album.id, url: row['Spotify'])
   else
     bad_rows << "index #{i}: #{row['Artist']}: #{row['Album']}"
   end
